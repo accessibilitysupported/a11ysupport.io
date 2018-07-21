55d5ca7a1dc78412a4e4b1382f5514f33ce9336a
Michael Fairchild <mfairchild365@gmail.com>
2018-07-21 14:42:14 -0500

    Import some aria features

--- scripts/generate-features.js
+++ scripts/generate-features.js
@@ -5,7 +5,7 @@ let moment = require('moment');
 
 let currentDateString = moment().format('YYYY-MM-DD');
 
-let createFeature = function(id, techId, title, description, linkTitle, linkUrl) {
+let createFeature = function(id, featureType, techId, title, description, linkTitle, linkUrl) {
 	//Ensure a constant and safe file name
 	id = id.replace(' ', '_').toLowerCase(); //Remove spaces
 	id = sanitize(id);
@@ -20,6 +20,7 @@ let createFeature = function(id, techId, title, description, linkTitle, linkUrl)
 	let feature = {
 		"id": id,
 		"title": title,
+		"type": featureType,
 		"description": "",
 		"references": [],
 		"tests": [],
@@ -49,14 +50,51 @@ fetch('https://raw.githubusercontent.com/w3c/elements-of-html/master/elements.js
 			}
 
 			// There are a LOT of elements, so lets reduce it to keep it simple for testing purposes
-			let temporary_whitelist = ['a', 'abbr', 'article', 'canvas', 'dialog', 'input[type="text"]', 'input[type="tel"]', 'progress'];
+			let temporary_whitelist = ['canvas', 'dialog', 'input[type="text"]', 'input[type="tel"]'];
 			if (!temporary_whitelist.includes(json[i].element)) {
 				continue;
 			}
 
 			let id = json[i].element + '_element';
-			let title = json[i].element + ' Element';
+			let title = json[i].element + ' element';
 
-			createFeature(id, 'html', title, null, 'HTML5 spec for ' + json[i].element, json[i].link);
+			createFeature(id, 'element', 'html', title, null, 'HTML5 spec for ' + json[i].element, json[i].link);
+		}
+	});
+
+// ARIA 1.1
+fetch('https://raw.githubusercontent.com/jamiebuilds/aria-data/master/data.json')
+	.then(res => res.json())
+	.then(function(json) {
+
+		// Temporary whitelist to keep things at a minimum during prototyping
+		let whitelist_roles = ['alert'];
+		let whitelist_attributes = ['aria-hidden'];
+
+		for (let key in json.roles) {
+			let role = json.roles[key];
+
+			if (!whitelist_roles.includes(role.name)) {
+				continue;
+			}
+
+			let id = role.name + '_role';
+			let title = role.name + ' role';
+
+			createFeature(id, 'role', 'aria', title, role.description, 'ARIA spec for ' + role.name, key);
+		}
+
+
+		for (let key in json.attributes) {
+			let attribute = json.attributes[key];
+
+			if (!whitelist_attributes.includes(attribute.name)) {
+				continue;
+			}
+
+			let id = attribute.name + '_attribute';
+			let title = attribute.name + ' attribute';
+
+			createFeature(id, 'attribute', 'aria', title, attribute.description, 'ARIA spec for ' + attribute.name, key);
 		}
 	});
