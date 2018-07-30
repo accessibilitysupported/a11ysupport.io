a7526438d4cf15dadd81cd6224b2c490ff10eb5d
Michael Fairchild <mfairchild365@gmail.com>
2018-07-29 20:04:54 -0500

    polyfill 'includes' to fix IE11

--- public/js/head.js
+++ public/js/head.js
@@ -1,3 +1,18 @@
+if (!String.prototype.includes) {
+	String.prototype.includes = function(search, start) {
+		'use strict';
+		if (typeof start !== 'number') {
+			start = 0;
+		}
+
+		if (start + search.length > this.length) {
+			return false;
+		} else {
+			return this.indexOf(search, start) !== -1;
+		}
+	};
+}
+
 var getJson = function(url, success, err) {
 	var request = new XMLHttpRequest();
 	request.open('GET', url, true);
