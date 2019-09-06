562ae47fe4a2196d3333caf8b13b42d8f1d015db
Michael Fairchild <mfairchild365@gmail.com>
2019-09-06 10:39:02 -0500

    set a canonical URL

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(function(req, res, next) {
+	app.locals.canonical = 'https://a11ysupport.io'+req.originalUrl;
+	return next();
+});
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
